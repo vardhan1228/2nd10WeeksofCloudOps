18cbe136094a578b73cfc07bfb9df9d20a647569
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-07-03 16:27:15 +0530

    Update Books.jsx

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -33,7 +33,7 @@ const Books = () => {
 
   return (
     <div>
-      <h1>Ankit book Shop</h1>
+      <h1>devops by verra </h1>
       <div className="books">
         {books.map((book) => (
           <div key={book.id} className="book">
