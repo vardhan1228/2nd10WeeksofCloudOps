69406cbaf387624a1f39023f5767384666be33b7
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-09-09 09:10:31 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,4 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
+//const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const API_BASE_URL = "http://api.narni.co.in";
 export default API_BASE_URL;
